24a0072ecd69980bdb540203ffe39f96660e6188
ostrovskyi <german.ostrovskij@gmail.com>
2023-11-19 21:50:14 +0100

    feat: add routes config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
-import {Suspense} from "react";
+import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/lib/useTheme";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
@@ -14,12 +12,7 @@ const App = () => {
             <button onClick={toggleTheme}>Change Theme</button>
             <Link to={'/'}>Main page</Link>
             <Link to={'/about'}>About page</Link>
-            <Suspense fallback={<div>...Loading</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     )
 }
